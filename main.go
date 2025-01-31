49a49c0c5438046b228301d993e21f518f913ab3
Davematteer <davematteer@gmail.com>
2025-01-31 23:44:35 +0000

    Starting go app

--- main.go
+++ main.go
@@ -11,6 +11,18 @@ type server struct {
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello bitches..."))
+
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("index page"))
+			return
+		case "/users":
+			w.Write([]byte("users page"))
+			return
+		}
+	}
 }
 
 func main() {
