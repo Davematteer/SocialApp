345c0af05d6713d4ef677f900a3d96a60a1a7f6d
Davematteer <davematteer@gmail.com>
2025-02-05 13:52:58 +0000

    adding persistance with postgres

--- internals/env/store/posts.go
+++ internals/env/store/posts.go
@@ -5,6 +5,16 @@ import (
 	"database/sql"
 )
 
+type Posts struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserID    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
 type PostsStore struct {
 	db *sql.DB
 }
