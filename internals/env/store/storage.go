05bc9630fb678f852ea6bb9fddd5454eed3f87f2
Davematteer <davematteer@gmail.com>
2025-02-05 13:40:09 +0000

    adding persistance with postgres

--- internals/env/store/storage.go
+++ internals/env/store/storage.go
@@ -15,7 +15,7 @@ type Storage struct {
 	}
 }
 
-func NewPostgresStorage(db *sql.DB) Storage {
+func NewStorage(db *sql.DB) Storage {
 	return Storage{
 		Post:  &PostsStore{db},
 		Users: &UsersStore{db},
